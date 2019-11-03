1ba6d7140b5ee58f8648d2aac22f84046d00e207
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-11-03 14:26:02 +0800

    Add data and setData hook

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
-import SearchBar from "./SearchBar/SearchBar";
 import "../styles/App.css";
 import axios from "axios";
+import SearchBar from "./SearchBar";
+import Display from "./Display";
 
 const App = () => {
   const [name, setName] = useState("");
+  const [data, setData] = useState("");
 
   const searchName = async name => {
     const response = await axios.get(
       `http://localhost:3001/api/people/?name=${name}`
     );
-    return response;
+    return setData(response.data);
   };
 
   return (
@@ -18,6 +20,7 @@ const App = () => {
       <header className="App-header">
         <h3>{name}</h3>
         <SearchBar name={name} setName={setName} searchName={searchName} />
+        {data && <Display data={data} />}
       </header>
     </div>
   );
