64d5ca1dd45488f1770b39324b644ebdde39ebbb
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-11-03 16:04:04 +0800

    Add comment to inform searchPersonName is not working

--- server/src/routes/people.route.js
+++ server/src/routes/people.route.js
@@ -5,6 +5,8 @@ const fetchAndStore = require("../utils/fetchAndStore");
 
 router.get("/", async (req, res) => {
   const name = req.query.name;
+  // something is wrong with searchPersonName, comment it and the if statement out
+  // to see fetchAndStore working
   let result = await searchPersonName(name);
   if (result.length === 0) {
     result = await fetchAndStore(name);
