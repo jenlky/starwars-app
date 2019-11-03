f6f35aee134c84f0c62cca0052d597f02f32ba83
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-11-03 14:26:43 +0800

    Add Display component

---
+++ client/src/components/Display.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Vehicles from "./Vehicles";
+import Starships from "./Starships";
+
+const Display = ({ data }) => {
+  return data.map(person => {
+    const { name, gender, homeworld, vehicles, starships } = person;
+    return (
+      <div className="display">
+        <p>Name: {name}</p>
+        <p>Gender: {gender}</p>
+        <ul>
+          <h3>Homeworld</h3>
+          <li key={homeworld.name}>Name: {homeworld.name}</li>
+          <li key={homeworld.population}>Population: {homeworld.population}</li>
+          <li key={homeworld.climate}>Climate: {homeworld.climate}</li>
+        </ul>
+        <Vehicles vehicles={vehicles} />
+        <Starships starships={starships} />
+      </div>
+    );
+  });
+};
+
+export default Display;
